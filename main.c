925222adc0129b882b77ab470d6465f7b90e6dec
zitraks <mdpershin@edu.hse.ru>
2025-01-28 00:11:22 +0300

    2d with dynamic malloc

--- main.c
+++ main.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-int* Dynamic2D(int n) {
-    int *p = (int*)malloc(5 * sizeof(int));
-    printf("Enter elemnts:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &p[i]);
+
+int* Dynamic2D(int* size) {
+    int capacity = 5;
+    int *p = (int*)malloc(sizeof(int) * capacity);
+    if (p == NULL) {
+        printf("Memory allocation error\n");
+        exit(EXIT_FAILURE);
     }
 
+    printf("Enter elements (0 to stop):\n");
+    while (1) {
+        int num;
+        scanf("%d", &num);
+        if (num == 0) {
+            break;
+        }
+        if (*size >= capacity) {
+            capacity *= 2;
+            p = (int*)realloc(p, capacity * sizeof(int));
+            if (p == NULL) {
+                printf("Memory allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        p[(*size)++] = num;
+    }
     return p;
 }
+
 int main(void) {
-    int *p = NULL;
-    p = Dynamic2D (5);
-    for (int i = 0; i < 5; i++) {
+    int size = 0;
+    int *p = Dynamic2D(&size);
+
+    printf("Elements are:\n");
+    for (int i = 0; i < size; i++) {
         printf("p[%d] = %d\n", i, p[i]);
     }
-    free(p);
 
-    if (p == NULL) {
-        printf("p is NULL\n");
-        exit(1);
-    }
-    else {
-        printf("p is valid\n");
-    }
+    free(p);
     return 0;
 }
