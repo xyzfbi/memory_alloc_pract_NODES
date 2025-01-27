30d3b5683097f7c07f1c9a7b98f98e558c40d825
zitraks <mdpershin@edu.hse.ru>
2025-01-27 23:16:54 +0300

    Some practise with array 1d

--- main.c
+++ main.c
@@ -9,5 +9,14 @@ int main(void)
     printf("Please enter a length of array: ");
     scanf("%d", &n);
     a = (int *)malloc(n * sizeof(int));
+    printf("Enter array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Array elements: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    free(a);
     return 0;
 }
