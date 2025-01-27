027cce181bf19e476693592ec7434002610898ca
zitraks <mdpershin@edu.hse.ru>
2025-01-27 23:13:28 +0300

    initbackward

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    int *a;
+    int n;
     printf("Hello, nigga!\n");
+    printf("Please enter a length of array: ");
+    scanf("%d", &n);
+    a = (int *)malloc(n * sizeof(int));
     return 0;
 }
